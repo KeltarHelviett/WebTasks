7e277d2587dcc0c2bf06321056f40421b0642ca1
KeltarHelviett <keltarhelviett@gmail.com>
2018-04-19 21:37:19 +1000

    Improved overall markup. Finished main render function

--- task_3/calendar.js
+++ task_3/calendar.js
@@ -19,7 +19,7 @@ Calendar.monthNames = ['January', 'February', 'March', 'April', 'May', 'June',
                        'July', 'August', 'September', 'October', 'November', 'December'];
 Calendar.mothShortNames = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June',
                            'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
-Calendar.shortWeekdayNames = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+Calendar.shortWeekdayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
 Calendar.prototype.init = function() {
     let self = this;
@@ -40,7 +40,7 @@ Calendar.prototype.init = function() {
 
     let headerElems = [
         {
-            element: 'i',
+            element: 'button',
             onclick: (e) => { self.prevMonth(); },
             class: 'calendar-header-prev-month',
             innerText: '<'
@@ -58,7 +58,7 @@ Calendar.prototype.init = function() {
             innerText: `${year}`
         },
         {
-            element: 'i',
+            element: 'button',
             onclick: (e) => { self.nextMonth(); },
             class: 'calendar-header-next-month',
             innerText: '>'
@@ -77,31 +77,50 @@ Calendar.prototype.init = function() {
     let weekdays = document.createElement('div');
     weekdays.classList.add('calendar-weekdays');
 
-    Calendar.shortWeekdayNames.forEach(function(item){
+    Calendar.shortWeekdayNames.forEach(function(item, index){
         let wd = document.createElement('div');
         wd.classList.add('calendar-weekday');
+        if (index == 0 || index == 6)
+            wd.classList.add('calendar-weekday-weekend');
         wd.innerText = item;
         weekdays.appendChild(wd);
     });
 
     this.calendarHTML.appendChild(weekdays);
-    // let prevMonthHTML = document.createElement('i');
-    // prevMonthHTML.innerText = '<';
-    // prevMonthHTML.classList.add('calendar-header-prev-month');
-    // prevMonthHTML.onclick = function(e) {
-    //     self.prevMonth();
-    // };
-
-    // header.appendChild(prevMonthHTML);
-
-    // ...
 
     this.carrier.appendChild(this.calendarHTML);
-    this.prevMonth();
+    self.render();
 }
 
 Calendar.prototype.render = function() {
+    let self = this;
+    // self.calendarHTML.removeChild(self.calendarHTML.lastChild);
 
+    let startDate = self.observingDate;
+    startDate.setDate(1);
+    startDate= new Date(startDate.getTime() - 86400000 * startDate.getDay());
+    startDate.setHours(0, 0, 0, 0);
+    let endDate = new Date(self.observingDate.getFullYear(), self.observingDate.getMonth() + 1, 0);
+    endDate = new Date(endDate.getTime() + 86400000 * (6 - endDate.getDay()));
+    
+    let calendarRows = document.createElement('div');
+    calendarRows.classList.add('calendar-rows');
+    self.calendarHTML.appendChild(calendarRows);
+    
+    let row = undefined;
+
+    while (startDate <= endDate) {
+        if (!startDate.getDay()) {
+            row = document.createElement('div');
+            row.classList.add('calendar-row');
+            calendarRows.appendChild(row);
+        }
+        let cell = document.createElement('div');
+        cell.classList.add('calendar-cell');
+        cell.innerText = startDate.getDate();
+        row.appendChild(cell);
+        startDate = new Date(startDate.getTime() + 86400000)
+    }
 }
 
 Calendar.prototype.nextMonth = function () {
